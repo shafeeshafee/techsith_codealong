9f2e4f5f472949875b4f277e602548cb4232eb66
Shafee <shafeelinks@gmail.com>
2021-02-04 05:42:50 -0500

    working version of techsith hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<button className="grow" onClick={this.props.onAgeUp}>
+					Grow
+				</button>
+				<div className="Age-label">
+					Age:&nbsp; <span>{this.props.age}</span>
+				</div>
+				<button className="antiage" onClick={this.props.onAgeDown}>
+					Anti-age
+				</button>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		age: state.age,
+	};
+};
+
+const mapDispachToProps = (dispatch) => {
+	return {
+		onAgeUp: () => dispatch({ type: "AGE_UP", value: 1 }),
+		onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 1 }),
+	};
+};
+export default connect(mapStateToProps, mapDispachToProps)(App);
